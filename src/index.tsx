6ae3f089d51fbc1e0e48758ef282d7c048c0a39c
JackAlgera <jack.algera@live.com>
2022-03-24 12:35:08 +0100

    Add proper interceptor

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,25 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+const axios = require('axios').default;
+const DEBUG = process.env.NODE_ENV === "development";
+
+const URL = 'http://localhost:8080/api'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+axios.interceptors.request.use((config: any) => {
+  config.url = `${URL}/${config.url}`;
+  return config;
+}, (error: any) => {
+  return Promise.reject(error);
+});
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
